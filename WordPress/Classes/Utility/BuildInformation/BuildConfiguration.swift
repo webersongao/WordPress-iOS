7fdc5526a0f4241cf3c922f32f7ddf838e33b763
Alex Grebenyuk <alex.grebenyuk@automattic.com>
2025-03-17 15:23:35 -0400

    Remove remaining internal build references (#24235)
    * Remove INTERNAL_BUILD compiler flag
    
    * Remove a8cPrereleaseTesting usages
    
    * Consolidate BuildConfiguration cases

--- WordPress/Classes/Utility/BuildInformation/BuildConfiguration.swift
+++ WordPress/Classes/Utility/BuildInformation/BuildConfiguration.swift
@@ -1,23 +1,18 @@
 enum BuildConfiguration: String {
-    /// Development build, usually run from Xcode
+    /// Development build, usually run from Xcode.
     case localDeveloper
 
-    /// Continuous integration builds for Automattic employees to test branches & PRs
-    case a8cBranchTest
+    /// Preproduction builds for Automattic employees.
+    case alpha
 
-    /// Beta released internally for Automattic employees
-    case a8cPrereleaseTesting
-
-    /// Production build released in the app store
+    /// Production build released in the app store.
     case appStore
 
     static var current: BuildConfiguration {
         #if DEBUG
             return .localDeveloper
         #elseif ALPHA_BUILD
-            return .a8cBranchTest
-        #elseif INTERNAL_BUILD
-            return .a8cPrereleaseTesting
+            return .alpha
         #else
             return .appStore
         #endif
@@ -28,6 +23,6 @@ enum BuildConfiguration: String {
     }
 
     var isInternal: Bool {
-        self ~= [.localDeveloper, .a8cBranchTest, .a8cPrereleaseTesting]
+        self ~= [.localDeveloper, .alpha]
     }
 }
