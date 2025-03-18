815edf2393ae680dbdc59abc3e3be6ab54500fe6
Alex Grebenyuk <alex.grebenyuk@automattic.com>
2025-03-18 12:03:36 -0400

    BuildSettingsKit v2 (#24233)
    * Add Bundle.app to BuildSettings
    
    * Remove duplicated values from Info.plist
    
    * Add BuildSettingsEnvironment
    
    * Add BuildSettingsEnvironment
    
    * Cleanup
    
    * Update DataMigratorTests
    
    * Disable StatsWidgetsStoreTests
    
    * Fix testExportFailsWhenSharedUserDefaultsNil
    
    * Re-enable StatsWidgetsStoreTests
    
    * Add support for swift-testing in BuildSettingsEnvironment

---
+++ Modules/Sources/BuildSettingsKit/BuildSettings+Live.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+extension BuildSettings {
+    static let live = BuildSettings(bundle: .app)
+
+    init(bundle: Bundle) {
+        pushNotificationAppID = bundle.infoValue(forKey: "WPPushNotificationAppID")
+        appGroupName = bundle.infoValue(forKey: "WPAppGroupName")
+        appKeychainAccessGroup = bundle.infoValue(forKey: "WPAppKeychainAccessGroup")
+    }
+}
+
+private extension Bundle {
+    func infoValue<T>(forKey key: String) -> T where T: LosslessStringConvertible {
+        guard let object = object(forInfoDictionaryKey: key) else {
+            fatalError("missing value for key: \(key)")
+        }
+        switch object {
+        case let value as T:
+            return value
+        case let string as String:
+            guard let value = T(string) else { fallthrough }
+            return value
+        default:
+            fatalError("unexpected value: \(object) for key: \(key)")
+        }
+    }
+}
+
+private extension Bundle {
+    /// Returns the `Bundle` for the host `.app`.
+    ///
+    /// - If this is called from code already located in the main app's bundle or from a Pod/Framework,
+    ///   this will return the same as `Bundle.main`, aka the bundle of the app itself.
+    /// - If this is called from an App Extension (Widget, ShareExtension, etc), this will return the bundle of the
+    ///   main app hosting said App Extension (while `Bundle.main` would return the App Extension itself)
+    static let app: Bundle = {
+        var url = Bundle.main.bundleURL
+        while url.pathExtension != "app" && url.lastPathComponent != "/" {
+            url.deleteLastPathComponent()
+        }
+        guard let appBundle = Bundle(url: url) else { fatalError("Unable to find the parent app bundle") }
+        return appBundle
+    }()
+}
---
+++ Modules/Sources/BuildSettingsKit/BuildSettings+Preview.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+/// The container for Xcode previews.
+extension BuildSettings {
+    nonisolated(unsafe) static var preview = BuildSettings(
+        pushNotificationAppID: "xcpreview_push_notification_id",
+        appGroupName: "xcpreview_app_group_name",
+        appKeychainAccessGroup: "xcpreview_app_keychain_access_group"
+    )
+}
+
+extension BuildSettings {
+    /// Updates the preview settings for the lifetime of the given closure.
+    /// Reverts to the original settings when done.
+    @MainActor
+    public static func withSettings<T>(_ configure: (inout BuildSettings) -> Void, perform closure: () -> T) -> T {
+        var container = BuildSettings.preview
+        let original = container
+        configure(&container)
+        BuildSettings.preview = container
+        let value = closure()
+        BuildSettings.preview = original
+        return value
+    }
+}
